caefe1a3b8c70a16d48172fda5c34ec71294436b
Bansa Laura <weidiwu207@gmail.com>
2021-02-12 12:24:36 -0500

    met mvp

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,44 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event'
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm/>);
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+
+    render(<CheckoutForm/>);
+
+    const firstName = screen.getByLabelText(/first name:/i);
+    const lastName = screen.getByLabelText(/last name:/i);
+    const address = screen.getByLabelText(/address:/i);
+    const city = screen.getByLabelText(/city:/i);
+    const state = screen.getByLabelText(/state:/i);
+    const zip = screen.getByLabelText(/zip:/i);
+    const button = screen.getByRole('button', {name: /checkout/i});
+
+
+    userEvent.type(firstName, "Wei");
+    userEvent.type(lastName, "Wu");
+    userEvent.type(address, "TotallyNotFakeAddress");
+    userEvent.type(city, "TotallyNotFakeCity");
+    userEvent.type(state, "TotallyNotFakeState");
+    userEvent.type(zip, "TotallyNotFakeZip");
+
+
+    expect(firstName).toHaveValue('Wei');
+    expect(lastName).toHaveValue('Wu');
+    expect(address).toHaveValue('TotallyNotFakeAddress');
+    expect(city).toHaveValue('TotallyNotFakeCity');
+    expect(state).toHaveValue('TotallyNotFakeState');
+    expect(zip).toHaveValue('TotallyNotFakeZip');
+
+    userEvent.click(button)
+
+    const successMsg = screen.getByText(/you have ordered some plants!/i)
+    expect(successMsg).toBeInTheDocument();
+});
